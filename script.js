6076f316ce88eafdd3ebb99428198a7e873a0113
james <admin@jamesbaber.co.uk>
2020-03-29 19:28:38 +0100

    Refactor JS

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-function httpGetAsync(theUrl, callback)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
+// HTTP GET request function
+function httpGetAsync(targetURL, callback) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() {
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+      callback(xmlHttp.responseText);
     }
-    xmlHttp.open("GET", theUrl, true);
-    xmlHttp.send(null);
+  }
+  xmlHttp.open("GET", targetURL, true);
+  xmlHttp.send(null);
 }
 
+// Update name on page randomly
 function newName() {
   httpGetAsync("names.txt", function(text) {
     var lines = text.split('\n');
@@ -16,9 +18,10 @@ function newName() {
     console.log(lines[num]);
     document.getElementById("name").innerHTML = lines[num]
   });
-
 }
 
+// Generate name on page load
 window.onload = newName;
 
+// Try and force landscape
 screen.orientation.lock("landscape");
